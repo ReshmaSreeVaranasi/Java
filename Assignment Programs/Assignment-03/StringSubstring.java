04557c5ed0754473814e4bdeb66af3c4a076173f
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 22:10:56 +0530

    Create StringSubstring.java

---
+++ Assignment Programs/Assignment-03/StringSubstring.java
@@ -0,0 +1,10 @@
+package javacore;
+
+public class StringSubstring {
+    public static void main(String[] args) {
+
+        String str = "Hello Java";
+
+        System.out.println(str.substring(6));
+    }
+}
