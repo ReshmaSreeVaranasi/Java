0bd6286e3ba7427ce4c262299947a3186fd5b90c
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 22:07:41 +0530

    Add StringCompare class for string comparison

---
+++ Assignment Programs/Assignment-03/StringCompare.java
@@ -0,0 +1,11 @@
+package javacore;
+
+public class StringCompare {
+    public static void main(String[] args) {
+
+        String str1 = "Apple";
+        String str2 = "Banana";
+
+        System.out.println(str1.compareTo(str2));
+    }
+}
