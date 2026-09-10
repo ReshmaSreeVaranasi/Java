9b6fa145c1fd878e41774cb265a5343794aa9819
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 22:08:35 +0530

    Create StringUpper.java

---
+++ Assignment Programs/Assignment-03/StringUpper.java
@@ -0,0 +1,10 @@
+package javacore;
+
+public class StringUpper {
+    public static void main(String[] args) {
+
+        String str = "hello";
+
+        System.out.println(str.toUpperCase());
+    }
+}
