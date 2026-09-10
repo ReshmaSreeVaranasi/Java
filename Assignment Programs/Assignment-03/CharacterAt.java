7ba89b2223fe42ffcd0bea8801077c503f683a6d
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 22:00:45 +0530

    Create CharacterAt.java

---
+++ Assignment Programs/Assignment-03/CharacterAt.java
@@ -0,0 +1,10 @@
+package javacore;
+
+public class CharacterAt {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        System.out.println(str.charAt(1));
+    }
+}
