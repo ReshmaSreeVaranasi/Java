15be19cb3c4e7e7782e9f5ce9750ee684ce22b1b
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 22:07:11 +0530

    Create StringEqualsIgnoreCase.java

---
+++ Assignment Programs/Assignment-03/StringEqualsIgnoreCase.java
@@ -0,0 +1,11 @@
+package javacore;
+
+public class StringEqualsIgnoreCase {
+    public static void main(String[] args) {
+
+        String str1 = "Hello";
+        String str2 = "HELLO";
+
+        System.out.println(str1.equalsIgnoreCase(str2));
+    }
+}
