eaefa6fb9f05c708ca91b4bdbd52d5f3cb1b3dad
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 22:15:57 +0530

    Add StringEndsWith class to check string endings

---
+++ Assignment Programs/Assignment-03/StringEndsWith.java
@@ -0,0 +1,10 @@
+package javacore;
+
+public class StringEndsWith {
+    public static void main(String[] args) {
+
+        String str = "Hello Java";
+
+        System.out.println(str.endsWith("Java"));
+    }
+}
