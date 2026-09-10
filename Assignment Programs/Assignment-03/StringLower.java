9ad75ddced18aa304e7f6abde4388e5aa75e6c8b
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 22:09:11 +0530

    Add StringLower class to convert string to lowercase

---
+++ Assignment Programs/Assignment-03/StringLower.java
@@ -0,0 +1,10 @@
+package javacore;
+
+public class StringLower {
+    public static void main(String[] args) {
+
+        String str = "HELLO";
+
+        System.out.println(str.toLowerCase());
+    }
+}
