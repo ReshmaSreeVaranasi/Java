32489b38ea9bce017485596b262778814f7640bf
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 21:57:10 +0530

    Add StringLength class to print string length

---
+++ Assignment Programs/Assignment-03/StringLength.java
@@ -0,0 +1,10 @@
+package javacore;
+
+public class StringLength {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        System.out.println("Length: " + str.length());
+    }
+}
