48e1d2f3d627443349a42682a71ef1b3920f9f68
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 22:04:18 +0530

    Create StringEquals.java

---
+++ Assignment Programs/Assignment-03/StringEquals.java
@@ -0,0 +1,11 @@
+package javacore;
+
+public class StringEquals {
+    public static void main(String[] args) {
+
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        System.out.println(str1.equals(str2));
+    }
+}
