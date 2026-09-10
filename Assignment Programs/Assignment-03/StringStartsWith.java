c4863d10e8da5367652c4fe16ea06d96e5164778
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-10 22:14:40 +0530

    Add StringStartsWith class to check string prefix

---
+++ Assignment Programs/Assignment-03/StringStartsWith.java
@@ -0,0 +1,10 @@
+package javacore;
+
+public class StringStartsWith {
+    public static void main(String[] args) {
+
+        String str = "Hello Java";
+
+        System.out.println(str.startsWith("Hello"));
+    }
+}
