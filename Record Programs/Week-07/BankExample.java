7392417abe50c9b5f8f7f37dbeaa2c3ee72246f1
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-15 22:45:17 +0530

    Create BankExample.java

---
+++ Record Programs/Week-07/BankExample.java
@@ -0,0 +1,64 @@
+
+package javacore;
+
+import java.util.Scanner;
+
+class RBI {
+    double rate = 4.0;
+
+    double getRate() {
+        return rate;
+    }
+}
+
+class SBI extends RBI {
+    double getRate() {
+        return 7.0;
+    }
+}
+
+class ICICI extends RBI {
+    double getRate() {
+        return 6.5;
+    }
+}
+
+class PNB extends RBI {
+    double getRate() {
+        return 6.0;
+    }
+}
+
+public class BankDemo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Bank name to find the rate of Interest : ");
+        String name = sc.nextLine();
+
+        RBI b;
+
+        if (name.equalsIgnoreCase("RBI")) {
+            b = new RBI();
+        } else if (name.equalsIgnoreCase("SBI")) {
+            b = new SBI();
+        } else if (name.equalsIgnoreCase("ICICI")) {
+            b = new ICICI();
+        } else if (name.equalsIgnoreCase("PNB")) {
+            b = new PNB();
+        } else {
+            System.out.println("Invalid bank name");
+            sc.close();
+            return;
+        }
+
+        System.out.println("RBI rate of interest is : " + b.getRate() + "%");
+
+        sc.close();
+    }
+}
+
+
+Enter the Bank name to find the rate of Interest : RBI
+RBI rate of interest is : 4.0%
