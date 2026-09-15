e21175f8aa365e5da41ac6eb944919b8be6d4e47
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-09-15 22:47:47 +0530

    Update BankExample.java

--- Record Programs/Week-07/BankExample.java
+++ Record Programs/Week-07/BankExample.java
@@ -29,7 +29,7 @@ class PNB extends RBI {
     }
 }
 
-public class BankDemo {
+public class BankExample {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
